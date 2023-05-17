b7f34625456061f4fcfb1c7917205a00a587a298
MrSalim86 <cph-ms1068@cphbusiness.dk>
2023-05-17 08:22:26 +0200

    last change

--- src/main/java/rest/RenameMeResource.java
+++ src/main/java/rest/RenameMeResource.java
@@ -15,7 +15,6 @@ import javax.ws.rs.core.MediaType;
 public class RenameMeResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
-    private static final ChuckNorrisFacade FACADECN = ChuckNorrisFacade.getChuckNorrisFacade(EMF);
     private static final FacadeExample FACADE = FacadeExample.getFacadeExample(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
