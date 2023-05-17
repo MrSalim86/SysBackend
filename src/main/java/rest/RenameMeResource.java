fe2d64571c3f6570ec8d59ce267851fd1d374a4c
MrSalim86 <cph-ms1068@cphbusiness.dk>
2023-05-17 08:18:56 +0200

    Ãslette weather and joke

--- src/main/java/rest/RenameMeResource.java
+++ src/main/java/rest/RenameMeResource.java
@@ -2,7 +2,6 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import facades.ChuckNorrisFacade;
 import utils.EMF_Creator;
 import facades.FacadeExample;
 import javax.persistence.EntityManagerFactory;
